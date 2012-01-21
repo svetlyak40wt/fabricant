328af8d3ebdb1a8588a685eb199d448188bd7abf
Alexander Artemenko <svetlyak.40wt@gmail.com>
2012-01-20 23:09:53 -0800

    Add function 'use_ssh_config' to use right identity files and ports for hosts.

--- fabricant/__init__.py
+++ fabricant/__init__.py
@@ -24,3 +24,30 @@ def make_symlinks(configs_dir='configs'):
                     )
                     if not os.path.lexists(symlink):
                         sudo('ln -s "{0}" "{1}"'.format(local_file, symlink))
+
+
+def use_ssh_config(env):
+    import ssh
+    import pdb;pdb.set_trace()
+    def hostinfo(host, config):
+        hive = config.lookup(host)
+        if 'hostname' in hive:
+            host = hive['hostname']
+        if 'user' in hive:
+            host = '%s@%s' % (hive['user'], host)
+        if 'port' in hive:
+            host = '%s:%s' % (host, hive['port'])
+        return host
+
+    try:
+        config_file = file(os.path.expanduser('~/.ssh/config'))
+    except IOError:
+        pass
+    else:
+        config = ssh.config.SSHConfig()
+        config.parse(config_file)
+        keys = [config.lookup(host).get('identityfile', None)
+            for host in env.hosts]
+        env.key_filename = [os.path.expanduser(key) for key in keys if key is not None]
+        env.hosts = [hostinfo(host, config) for host in env.hosts]
+
